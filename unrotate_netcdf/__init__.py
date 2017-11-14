daf791c31113bce6550d84ccb96f8ebff3f5fc38
Jonas Sølvsteen <j08lue@gmail.com>
2017-11-14 13:43:12 +0100

    don't actually shift longitude values

--- unrotate_netcdf/__init__.py
+++ unrotate_netcdf/__init__.py
@@ -20,23 +20,27 @@ TIME_ENCODING = {
     'dtype': 'float64'}
 
 
-def unrotate_ds(ds):
+def unrotate_ds(ds, actually_rotate=False):
     """Convert Dataset from roated-pole (lon only) to regular lat/lon"""
-    rp = ds['rotated_pole']
+    try:
+        rp = ds['rotated_pole']
+    except ValueError:
+        raise ValueError('Dataset contains not variable `rotated_pole`.')
     assert rp.attrs['grid_mapping_name'] == 'rotated_latitude_longitude'
     assert rp.attrs['grid_north_pole_latitude'] == 90
     lon_offset = rp.attrs['grid_north_pole_longitude']
 
+    # rotate longitudes
     dsnew = ds.rename(TO_RENAME).drop('rotated_pole')
+    if actually_rotate:
+        dsnew.coords['lon'] = ds['rlon'] + lon_offset
+        dsnew = dsnew.swap_dims({'rlon': 'lon'}).drop('rlon')
 
-    dsnew['lon'] += lon_offset
-
+    # modify attributes
     for da in dsnew.data_vars.values():
         da.attrs.pop('grid_mapping')
-
     for varn in ['lat', 'lon']:
         dsnew[varn].attrs = LATLON_ATTRIBUTES[varn]
-
     dsnew.attrs = {'Conventions': 'CF-1.7'}
     return dsnew
 
